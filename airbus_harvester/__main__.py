21b2f8cb57514d4ce88b9443290b995f0336911a
hcollingwood <hannah.collingwood@telespazio.com>
2024-10-21 16:46:20 +0100

    EODHP-785 - Fix tests

--- airbus_harvester/__main__.py
+++ airbus_harvester/__main__.py
@@ -164,11 +164,11 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
 
     upload_file_s3(json.dumps(latest_harvested), s3_bucket, metadata_s3_key, s3_client)
 
+    if latest_harvested.get(collection_key) == previously_harvested.get(collection_key):
+        all_keys["updated_keys"].discard(collection_key)
+
     # Remove this otherwise it will be marked for deletion
     previously_harvested.pop(collection_key, None)
-
-    if latest_harvested[collection_key] == previously_harvested[collection_key]:
-        all_keys["updated_keys"].discard(collection_key)
 
     deleted_keys = list(previously_harvested.keys())
     output_data = {
