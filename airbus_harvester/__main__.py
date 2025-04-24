d347bb44b66edb9d54df951ad08af3d0ee81122f
hcollingwood <hannah.collingwood@telespazio.com>
2025-04-24 16:01:53 +0100

    EODHP-1099 - updates to old collection data

--- airbus_harvester/__main__.py
+++ airbus_harvester/__main__.py
@@ -126,6 +126,20 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
     collection_key = f"{key_root}/collections/{config['collection_name']}.json"
     current_harvest_keys.add(collection_key)
 
+    is_first_harvest = True
+    old_collection_data = get_metadata(s3_bucket, collection_key, s3_client)
+    if old_collection_data:
+        is_first_harvest = False
+        start_time = old_collection_data['extent']['temporal']['interval'][0][0]
+        stop_time = old_collection_data['extent']['temporal']['interval'][0][1]
+        datetime_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+        bbox = data['extent']['spatial']['bbox'][0]
+        coordinates = [[bbox[1], bbox[0]], [bbox[3], bbox[2]]]
+
+        old_catalogue_data_summary = {"start_time": [datetime.datetime.strptime(start_time, datetime_format)], "stop_time": [datetime.datetime.strptime(stop_time, datetime_format)], "coordinates": coordinates}
+        logging.info(f"Previous harvest data recovered: {old_catalogue_data_summary}")
+
     catalogue_data_summary = current_harvest_metadata.get("summary")
     if not catalogue_data_summary:
         catalogue_data_summary = {"start_time": [], "stop_time": [], "coordinates": []}
@@ -163,8 +177,10 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
                 logging.error(f"Invalid entry in {next_url}")
 
             catalogue_data_summary = add_to_catalogue_data_summary(catalogue_data_summary, data)
+            old_catalogue_data_summary = add_to_catalogue_data_summary(old_catalogue_data_summary, data)
 
         catalogue_data_summary = simplify_catalogue_data_summary(catalogue_data_summary)
+        old_catalogue_data_summary = simplify_catalogue_data_summary(old_catalogue_data_summary)
 
         if config["pagination_method"] == "link":
             try:
@@ -185,7 +201,10 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
 
         logging.info(f"Page {url_count} next URL: {next_url}")
 
-        summary = get_stac_collection_summary(catalogue_data_summary)
+        if is_first_harvest:
+            summary = get_stac_collection_summary(catalogue_data_summary)
+        else:
+            summary = get_stac_collection_summary(old_catalogue_data_summary)
 
         # Collection updates every loop so that start/stop times and bbox values are the latest
         # ones from the Airbus catalogue
@@ -196,7 +215,7 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
         )
 
         file_hash = get_file_hash(json.dumps(collection_data))
-        if not previous_hash or previous_hash != file_hash:
+        if is_first_harvest and not previous_hash or previous_hash != file_hash:
             # Data was not harvested previously
             logging.info(f"Added: {collection_key}")
             harvested_data[collection_key] = collection_data
@@ -225,9 +244,14 @@ def harvest(workspace_name: str, catalog: str, s3_bucket: str):
             harvested_data = {}
             latest_harvested = {}
 
-    # Do not update collection
-    if latest_harvested.get(collection_key) == current_harvest_metadata.get(collection_key):
-        harvested_data.discard(collection_key)
+ 
+    summary = get_stac_collection_summary(catalogue_data_summary)
+    collection_data = generate_stac_collection(summary, config)
+    file_hash = get_file_hash(json.dumps(collection_data))
+
+    logging.info(f"Added: {collection_key}")
+    harvested_data[collection_key] = collection_data
+    latest_harvested[collection_key] = file_hash
 
     logging.info(f"Adding final keys: {len(current_harvest_metadata)} items")
     for key, value in latest_harvested.items():
@@ -409,7 +433,7 @@ def get_file_hash(data: str) -> str:
     return _md5_hash(data.encode("utf-8"))
 
 
-def get_metadata(bucket: str, key: str, s3_client: boto3.client) -> dict:
+def get_file_data(bucket: str, key: str, s3_client: boto3.client) -> dict:
     """Read file at given S3 location and parse as JSON"""
     previously_harvested = get_file_s3(bucket, key, s3_client)
     try:
